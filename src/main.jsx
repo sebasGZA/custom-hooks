1b5044ab1d6ca7e4a0c00fa12f7abb4e44fc9827
sebasGZA <sebassga34@gmail.com>
2023-03-26 15:53:06 -0500

    First commit custom hooks

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <>
+      <h1>Custom Hooks</h1>
+      <hr />
+    </>
+  </React.StrictMode>
+);
